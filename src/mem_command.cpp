e7cfbf513dbabb34d6f3a9e0ed6de2a82753b0fc
tnagy1024 <tnagy1024@da83a926-4c8c-11de-9241-1f6e7bca54ae>
2010-12-24 00:16:25 +0000

    more changes ahead
    git-svn-id: https://semantik.googlecode.com/svn/trunk@66 da83a926-4c8c-11de-9241-1f6e7bca54ae

---
+++ src/mem_command.cpp
@@ -0,0 +1,5 @@
+#include "mem_command.h"
+
+mem_command::mem_command(void* src) {
+	source = src;
+}
---
+++ src/mem_command.h
@@ -0,0 +1,13 @@
+
+#ifndef MEM_COMMAND
+#define MEM_COMMAND
+
+class mem_command {
+	public:
+		mem_command(void* source);
+		void* source;
+};
+
+
+#endif
+
