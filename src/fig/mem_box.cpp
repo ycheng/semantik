08d992d47d6499ffc93c609f279346a7e3e5375c
tnagy1024 <tnagy1024@da83a926-4c8c-11de-9241-1f6e7bca54ae>
2011-12-07 01:19:14 +0000

    new files
    git-svn-id: https://semantik.googlecode.com/svn/trunk@199 da83a926-4c8c-11de-9241-1f6e7bca54ae

---
+++ src/fig/mem_box.cpp
@@ -0,0 +1,49 @@
+// Thomas Nagy 2007-2011 GPLV3
+
+#include <QDebug>
+#include <sonnet/dialog.h>
+/*
+  */ %: include "con.h"  
+        #include <stdlib.h> 
+ #include "sembind.h"  	
+  #include "data_item.h"	
+  %: include "sem_model.h" 
+
+#include "mem_box.h"
+
+mem_del_box::mem_del_box(sem_model* mod) : mem_command(mod) {
+
+}
+
+void mem_del_box::init(int id, QList<int> ids) {
+
+}
+
+
+void mem_del_box::undo() {}
+void mem_del_box::redo() {}
+
+mem_add_box::mem_add_box(sem_model* mod) : mem_command(mod) {
+	item = NULL;
+}
+
+void mem_add_box::init(int id) {
+	item = model->m_oItems[id];
+	box = new data_box(model->next_seq());
+}
+
+void mem_add_box::redo()
+{
+	item->m_oBoxes[box->m_iId] = box;
+	model->notify_add_box(item->m_iId, box->m_iId);
+	redo_dirty();
+}
+
+void mem_add_box::undo()
+{
+	model->notify_del_box(item->m_iId, box->m_iId);
+	item->m_oBoxes.remove(box->m_iId);
+	undo_dirty();
+}
+
+
---
+++ src/fig/mem_box.h
@@ -0,0 +1,39 @@
+// Thomas Nagy 2007-2011 GPLV3
+
+#ifndef MEM_BOX
+#define MEM_BOX
+
+#include "mem_command.h"
+
+class mem_del_box : public mem_command {
+	public:
+		mem_del_box(sem_model*);
+		void init(int id, QList<int> ids);
+		void undo();
+		void redo();
+
+		//QList<data_item*> items;
+		//QSet<QPoint> links;
+
+		IType type() { return DEL_BOX; }
+};
+
+
+class mem_add_box : public mem_command {
+	public:
+		mem_add_box(sem_model*);
+		void init(int id);
+		void undo();
+		void redo();
+
+		data_item* item;
+		data_box* box;
+		int parent;
+
+		double m_iXX;
+		double m_iYY;
+
+		IType type() { return ADD_BOX; }
+};
+
+#endif
