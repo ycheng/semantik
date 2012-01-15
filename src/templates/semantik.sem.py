508eca4c0e88aec3310959a398644d5bdc253eb8
tnagy1024 <tnagy1024@da83a926-4c8c-11de-9241-1f6e7bca54ae>
2012-01-15 23:13:26 +0000

    cleanup
    git-svn-id: https://semantik.googlecode.com/svn/trunk@326 da83a926-4c8c-11de-9241-1f6e7bca54ae

--- src/templates/semantik.sem.py
+++ src/templates/semantik.sem.py
@@ -22,15 +22,17 @@ lst = os.listdir('.')
 tmp = str(sembind.get_item_ids()).split(",")
 for x in tmp:
 	item = sembind.get_item_by_id(int(x))
-	if not item:
-		debug("Could not get an object for %r" % x)
+	if item:
+		pid = sembind.get_val(item, "pic_id")
+		s = 'img-%s' % pid
+		if pid != '0':
+			for pic in lst:
+				if pic.startswith(s):
+					tar.add(pic, pic)
+					break
+			else:
+				debug("Could not find the picture for item %r->%r in %r" % (x, pid, sembind.get_var('temp_dir')))
 	else:
-		s = 'img-%s' % sembind.get_val(item, "pic_id")
-		for pic in lst:
-			if pic.startswith(s):
-				tar.add(pic, pic)
-				break
-		else:
-			debug("Could not find the picture %r in %r" % (x, sembind.get_var('temp_dir')))
+		debug("Could not get an object for %r" % x)
 tar.close()
 
