f49277541292bfcb22f19c4bb57ad6d274a5f78c
tnagy1024 <tnagy1024@da83a926-4c8c-11de-9241-1f6e7bca54ae>
2012-08-26 08:30:34 +0000

    fix one problem with the link creation
    git-svn-id: https://semantik.googlecode.com/svn/trunk@483 da83a926-4c8c-11de-9241-1f6e7bca54ae

--- src/fig/box_view.cpp
+++ src/fig/box_view.cpp
@@ -1287,6 +1287,8 @@ void box_view::mouseReleaseEvent(QMouseEvent *i_oEv)
 			delete m_oCurrent;
 			m_oCurrent = NULL;
 		}
+		QGraphicsView::mouseReleaseEvent(i_oEv);
+                m_bPressed = false;
 		return;
 	}
 
